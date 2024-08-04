8de3e042b2ec1ffcd61ba9e087ba68e310f93f63
Sergey020 <114489697+Sergey020@users.noreply.github.com>
2024-08-04 15:06:59 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -39,44 +39,6 @@ export function setupServer() {
  app.use(errorHendler);
 
 
-  // app.get('/contacts', async (req, res) => {
-  //   const contacts = await getAllcontacts();
-  //   res.status(200).json({
-  //     status: 200,
-  //     message: 'Successfully found contacts!',
-  //     data: contacts,
-  //   });
-  // });
-
-
-  // app.get('/contacts/:contactId', async (req, res, next) => {
-  //   const { contactId } = req.params;
-  //   const contact = await getContactById(contactId);
-
-  //   if (!contact) {
-  //     res.status(404).json({
-  //       message: 'Contact not found',
-  //     });
-  //     return;
-  //   }
-  //   res.status(200).json({
-  //     status: 200,
-  //     message: 'Successfully found contact with id {**contactId**}!',
-  //     data: contact,
-  //   });
-  // });
-
-
-  // app.use('*', (req, res, next) => {
-  //   res.status(404).send({ status: 404, message: 'Route not found' });
-  // });
-
- 
-  // app.use((error, req, res, next) => {
-  //   console.error(error);
-  //   res.status(500).send({ status: 500, message: 'Internal Server Error' });
-  // });
-
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
