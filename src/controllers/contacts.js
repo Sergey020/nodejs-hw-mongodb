20cf54fcf63c23f77d1e94608ab8e276836d8565
Sergey020 <114489697+Sergey020@users.noreply.github.com>
2024-08-18 18:49:55 +0300

    pagination
    prettier

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,15 +1,25 @@
-import { deleteContact, getAllcontacts, getContactById, updateContact, } from '../services/contacts.js';
+import {
+  deleteContact,
+  getAllcontacts,
+  getContactById,
+  updateContact,
+} from '../services/contacts.js';
 import createHttpError from 'http-errors';
-import { createContact} from '../services/contacts.js';
+import { createContact } from '../services/contacts.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
 export const getContactsController = async (req, res) => {
-    const contacts = await getAllcontacts();
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  };
+  const { page, perPage } = parsePaginationParams(req.query);
+  const contacts = await getAllcontacts({
+    page,
+    perPage,
+  });
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
 
 // eslint-disable-next-line no-unused-vars
 export const getContactByIdController = async (req, res, next) => {
@@ -27,7 +37,7 @@ export const getContactByIdController = async (req, res, next) => {
   });
 };
 
-export const createContactController  = async (req,res) => {
+export const createContactController = async (req, res) => {
   const contact = await createContact(req.body);
   res.status(200).json({
     status: 201,
@@ -37,26 +47,26 @@ export const createContactController  = async (req,res) => {
 };
 
 // eslint-disable-next-line no-unused-vars
-export const deleteContactController = async (req, res,next)=> {
+export const deleteContactController = async (req, res, next) => {
   const { contactId } = req.params;
   const contact = await deleteContact(contactId);
-  
-  if(!contact){
+
+  if (!contact) {
     throw createHttpError(404, 'Contact not found');
   }
   res.status(202).send();
 };
 
 // eslint-disable-next-line no-unused-vars
-export const pathContactController = async (req, res,next) => {
-const { contactId } = req.params;
-const contact = await updateContact(contactId,req.body);
-if(!contact) {
-  throw createHttpError(404, 'Contact not found');
-}
-res.json({
-  status: 200,
-  message:`Successfully patched a contact`,
-  data: contact.contact,
-});
+export const pathContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await updateContact(contactId, req.body);
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
+  }
+  res.json({
+    status: 200,
+    message: `Successfully patched a contact`,
+    data: contact.contact,
+  });
 };
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,22 @@
 import { ContactsCollection } from '../db/models/contacts.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllcontacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
+export const getAllcontacts = async ({ page, perPage }) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = ContactsCollection.find();
+  const contactsCount = await ContactsCollection.find()
+    .merge(contactsQuery)
+    .countDocuments();
+
+  const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactById = async (contactId) => {
@@ -14,22 +28,21 @@ export const getContactById = async (contactId) => {
 //return Contact.create(payload);
 //}
 
-
 export const createContact = async (payload) => {
   const contact = await ContactsCollection.create(payload);
   return contact;
 };
 
-export const updateContact = async ( contactId, payload)=> {
+export const updateContact = async (contactId, payload) => {
   const rawResult = await ContactsCollection.findOneAndUpdate(
-{_id:contactId},
-payload,
-{
-  new: true,
-  includeResultMetadata: true,
-},
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+    },
   );
-  if(!rawResult || !rawResult.value) return null;
+  if (!rawResult || !rawResult.value) return null;
 
   return {
     contact: rawResult.value,
