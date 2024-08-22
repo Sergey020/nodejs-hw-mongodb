abae5ea43f166f09739bfcef82c211e496c9998d
Sergey020 <114489697+Sergey020@users.noreply.github.com>
2024-08-22 18:55:03 +0300

    logoutUser

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,6 +2,7 @@
 import { registerUser } from '../services/auth.js';
 import { loginUser } from '../services/auth.js';
 import { ONE_DAY } from '../constants/index.js';
+import { logoutUser } from '../services/auth.js';
 
 
 export const registerUserController = async (req, res) => {
@@ -33,5 +34,15 @@ export const registerUserController = async (req, res) => {
         accessToken: session.accessToken,
       },
     });
-
+  };
+   
+  export const logoutUserController = async (req, res) => {
+    if (req.cookies.sessionId) {
+      await logoutUser(req.cookies.sessionId);
+    }
+  
+    res.clearCookie('sessionId');
+    res.clearCookie('refreshToken');
+  
+    res.status(204).send();
   };
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -6,6 +6,7 @@ import { validateBody } from '../middlewares/validateBody.js';
 
 import { loginUserSchema } from "../validation/auth.js";
 import { loginUserController } from "../controllers/auth.js";
+import { logoutUserController } from "../controllers/auth.js";
 
 
 const router = Router();
@@ -20,6 +21,8 @@ router.post(
     '/login',
     validateBody(loginUserSchema),
     ctrWrapper(loginUserController),
-)
+);
+
+router.post('/logout', ctrWrapper(logoutUserController));
 
 export default router;
